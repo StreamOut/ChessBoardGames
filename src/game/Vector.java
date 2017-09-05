f7adc78a9e1288759fbf4ac4bb4f666fc30540c6
malo <malodumont@gmail.com>
2017-09-05 18:24:57 +0200

    Création de Vector et Piece

---
+++ src/game/Vector.java
@@ -0,0 +1,14 @@
+package game;
+
+public class Vector {
+	private int [] way = new int[8];
+
+	public int[] getWay() {
+		return way;
+	}
+
+	public void setWay(int[] way) {
+		this.way = way;
+	}
+
+}

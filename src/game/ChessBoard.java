5b949c5664662dd1216d252ac71e495a0a7b9bd5
malo <malodumont@gmail.com>
2017-09-05 18:45:56 +0200

    Basic Display

--- src/game/ChessBoard.java
+++ src/game/ChessBoard.java
@@ -1,8 +1,20 @@
 package game;
 
 public class ChessBoard {
+	
+	public void display (){
+//		for(int i = 0; i < 8; i++)
+//			System.out.print(" _");
+		System.out.println();
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				System.out.print("| ");
+			}
+			System.out.println("|");
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("lol");
+		new ChessBoard().display();
 	}
 }
--- src/game/Piece.java
+++ src/game/Piece.java
@@ -1,5 +1,12 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Piece {
+	private String id;
+	private String name;
+	private ArrayList<Vector> position;
+	
+	
 
 }
